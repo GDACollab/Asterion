033fa7fc0f0afad03ce63a97f205b68873a3ceca
Jonah Ryan <87236765+jonahrobot@users.noreply.github.com>
2022-02-15 21:27:27 -0600

    Missile Firing and Tracing

---
+++ Assets/Scripts/Enemy_AI/scr_missile_move.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * Missile heat seeking movement.
+ *
+ * Developer: Jonah Ryan
+ */
+
+public class scr_missile_move : MonoBehaviour
+{
+    public Vector2 playerPos;
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
