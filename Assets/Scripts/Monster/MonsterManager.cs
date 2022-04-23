a43754edf38997fa2aaf0cb32a732d0e80aa4bc1
Calex Raffield <clxrffdman@gmail.com>
2022-04-23 11:48:20 -0700

    - added spawning monster functionality

--- Assets/Scripts/Monster/MonsterManager.cs
+++ Assets/Scripts/Monster/MonsterManager.cs
@@ -6,6 +6,12 @@ public class MonsterManager : MonoBehaviour
 {
 
     public PlayerRoomDetection playerRoomDetection;
+    [SerializeField] private Transform player;
+    [SerializeField] private GameObject monsterPrefab;
+    [SerializeField] private List<Transform> asterionMonsterLocations;
+    [SerializeField] private List<Transform> astramoriMonsterLocations;
+    [SerializeField] private List<Transform> hallwayMonsterLocations;
+    public List<GameObject> currentlyActiveMonsterSpawns;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +22,10 @@ public class MonsterManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            SpawnMonsterBehindCurrentRoomMachine();
+        }
     }
 
     public void UpdatedPlayerPos()
@@ -24,6 +33,35 @@ public class MonsterManager : MonoBehaviour
 
     }
 
-    
+    public void SpawnMonsterBehindCurrentRoomMachine()
+    {
+        if (playerRoomDetection.playerLocation == PlayerRoomDetection.Location.AsterionRoom)
+        {
+            var monster = Instantiate(monsterPrefab, asterionMonsterLocations[0].position, Quaternion.identity);
+            monster.transform.eulerAngles = new Vector3(90, -180, 0);
+            monster.transform.parent = transform;
+            currentlyActiveMonsterSpawns.Add(monster);
+            StartCoroutine(DestroyMonstersRoutine(3));
+        }
+        else if (playerRoomDetection.playerLocation == PlayerRoomDetection.Location.AstramoriRoom)
+        {
+            var monster = Instantiate(monsterPrefab, astramoriMonsterLocations[0].position, Quaternion.identity);
+            monster.transform.eulerAngles = new Vector3(90, -180, 180);
+            monster.transform.parent = transform;
+            currentlyActiveMonsterSpawns.Add(monster);
+            StartCoroutine(DestroyMonstersRoutine(3));
+        }
+    }
+
+    public IEnumerator DestroyMonstersRoutine(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+
+        foreach(GameObject g in currentlyActiveMonsterSpawns)
+        {
+            currentlyActiveMonsterSpawns.Remove(g);
+            Destroy(g);
+        }
+    }
     
 }
