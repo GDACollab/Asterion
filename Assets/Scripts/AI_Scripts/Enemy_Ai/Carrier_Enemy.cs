1a67e076104651e9b7552a4576923c3a32889f67
SalilPT <32131426+SalilPT@users.noreply.github.com>
2022-04-23 17:54:07 -0700

    Changed how fighters are spawned by carriers

--- Assets/Scripts/AI_Scripts/Enemy_Ai/Carrier_Enemy.cs
+++ Assets/Scripts/AI_Scripts/Enemy_Ai/Carrier_Enemy.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System; // Included here to use an anonymous function
+
 /*
 Edit this object in the Unity UI at Assets/Prefabs/Enemy_Ai/obj_carrier.prefab
 Note: Might want to add a death animation in the future
 
 BUGS:
-Fighters spawned in the same exact position are bad
+Spawned fighters persist after Astramori finishes
 
 Created by:
 - SalilPT
@@ -23,57 +25,50 @@ namespace AsterionArcade
         public double explodeDist = 4;
         [Tooltip("The number of fighters to spawn when this enemy is destroyed.")]
         public uint numFightersToSpawn = 5;
-        // The transform component for ships to spawn under.
-        private Transform shipsTransform;
         // Used to determine whether or not this object was destroyed because it was close enough to the player
         private bool inRangeOfPlayer = false;
-
-        // Is this object currently in Astramori?
-        private bool inAstramori;
-
-        void Awake()
-        {
-            inAstramori = this.GetComponent<Enemy>().isAstramori;
-            // Set the transform component that fighters will spawn as a child of to be the parent of this object
-            shipsTransform = transform.parent;
-        }
 
         // Update is called every frame
         void Update()
         {
-            
             Vector2 myPos = transform.position;
             // Check if this object is in range of the player to destroy itself. If it is, then destroy self.
             // Player position comes from Enemy.cs
             if (Vector2.Distance(myPos, player.transform.position) <= explodeDist)
             {
-                Debug.Log("CARRIER IN RANGE");
                 inRangeOfPlayer = true;
                 Destroy(this.gameObject);
             }
-            
         }
         
         // Method to determine the spawn positions of the fighters that this object spawns
-        List<Vector3> populateListOfSpawnPositions()
+        private List<Vector3> generateListOfSpawnPositions()
         {
             List<Vector3> result = new List<Vector3>();
+            Vector3 myPos = (Vector3)transform.position;
+            
+            result.Add(myPos);
 
-            result.Add((Vector3)transform.position);
-            result.Add((Vector3)transform.position);
-            result.Add((Vector3)transform.position);
-            result.Add((Vector3)transform.position);
-            result.Add((Vector3)transform.position);
+            Quaternion rotationQuat = transform.rotation;
+            // Anonymous function which returns the provided point rotated about the center of this object (on the play area)
+            // Adapted from the answer here: https://forum.unity.com/threads/rotate-a-point-around-a-second-point-using-a-quaternion.504996/
+            Func<Vector3, Vector3> generateRotatedPoint = (p) => {return (rotationQuat * (p - myPos) + myPos);};
+            
+            result.Add(generateRotatedPoint(myPos + new Vector3(-0.5f, -0.5f, 0)));
+            result.Add(generateRotatedPoint(myPos + new Vector3(-0.5f, 0.5f, 0)));
+            result.Add(generateRotatedPoint(myPos + new Vector3(0.5f, -0.5f, 0)));
+            result.Add(generateRotatedPoint(myPos + new Vector3(0.5f, 0.5f, 0)));
+            
             return result;
         }
         
-        // Spawns a fighter. Similar to the SpawnShip method in Spawning.cs .
-        private void SpawnFighter(Vector3 position) {
+        // Spawns a fighter as a chils of shipsTransform. Similar to the SpawnShip method in Spawning.cs .
+        private void SpawnFighter(Vector3 position, Transform shipsTransform) {
             int fighterShipID = 0;
             GameObject newFighter = Instantiate(GameManager.Instance.alienShipPrefabs[fighterShipID], shipsTransform);
 
-            // Spawn the new fighter on the correct layer
-            newFighter.layer = inAstramori ? 12 : 7;
+            // Spawn the new fighter on the same layer as this object
+            newFighter.layer = this.gameObject.layer;
 
             newFighter.transform.position = position * Vector2.one;
 
@@ -82,7 +77,7 @@ namespace AsterionArcade
                 ship1.lookingForPlayer = true;
             }
 
-            newFighter.GetComponent<Enemy>().isAstramori = true;
+            newFighter.GetComponent<Enemy>().isAstramori = isAstramori;
         }
 
         // When this object is destroyed, call this function
@@ -90,17 +85,18 @@ namespace AsterionArcade
         {
             // Get the health of this object through the fighter_enemy_health script attached to it
             int myHealth = this.GetComponent<fighter_enemy_health>().health;
-            Debug.Log(myHealth);
             // Need to check cause of destruction
             if ((myHealth <= 0) || inRangeOfPlayer)
                 {
-                List<Vector3> listOfSpawnPositions = populateListOfSpawnPositions();
+                List<Vector3> listOfSpawnPositions = generateListOfSpawnPositions();
+                // Set the transform component that fighters will spawn as a child of to be the parent of this object
+                Transform shipsTransform = transform.parent;
+                
                 for (int i = 0; i < numFightersToSpawn; i++)
                 {
-                    this.SpawnFighter(listOfSpawnPositions[i]);
+                    this.SpawnFighter(listOfSpawnPositions[i], shipsTransform);
                 }
             }
         }
-
     }
 }
