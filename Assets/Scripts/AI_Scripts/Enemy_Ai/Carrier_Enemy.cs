9496676daee0e636f3338868e863258f69c5b1ae
SalilPT <32131426+SalilPT@users.noreply.github.com>
2022-04-27 10:08:39 -0700

    Changed placeholder sprite for carrier and modified Carrier_Enemy.cs

--- Assets/Scripts/AI_Scripts/Enemy_Ai/Carrier_Enemy.cs
+++ Assets/Scripts/AI_Scripts/Enemy_Ai/Carrier_Enemy.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using System; // Included here to use an anonymous function
-
 /*
 Edit this object in the Unity UI at Assets/Prefabs/Enemy_Ai/obj_carrier.prefab
 Note: Might want to add a death animation in the future
 
 BUGS:
-Spawned fighters' bullets may persist after Astramori finishes
+- Spawned fighters' bullets may persist after Astramori finishes. This is a problem with AstramoriManager.cs where enemy bullets aren't cleared at the end of the game.
 
 Created by:
 - SalilPT
@@ -40,29 +38,35 @@ namespace AsterionArcade
                 Destroy(this.gameObject);
             }
         }
-        
+
         // Method to determine the spawn positions of the fighters that this object spawns
+        // You can change this to create different spawn patterns
         private List<Vector3> generateListOfSpawnPositions()
         {
             List<Vector3> result = new List<Vector3>();
             Vector3 myPos = (Vector3)transform.position;
-            
-            result.Add(myPos);
 
             Quaternion rotationQuat = transform.rotation;
             // Anonymous function which returns the provided point rotated about the center of this object (on the play area)
             // Adapted from the answer here: https://forum.unity.com/threads/rotate-a-point-around-a-second-point-using-a-quaternion.504996/
-            Func<Vector3, Vector3> generateRotatedPoint = (p) => {return (rotationQuat * (p - myPos) + myPos);};
-            
+            System.Func<Vector3, Vector3> generateRotatedPoint = (p) => {return (rotationQuat * (p - myPos) + myPos);};
+
+            // Spawn 1 fighter at the center of this object and 4 fighters equidistant to the center
+            result.Add(myPos);
             result.Add(generateRotatedPoint(myPos + new Vector3(-0.5f, -0.5f, 0)));
             result.Add(generateRotatedPoint(myPos + new Vector3(-0.5f, 0.5f, 0)));
             result.Add(generateRotatedPoint(myPos + new Vector3(0.5f, -0.5f, 0)));
             result.Add(generateRotatedPoint(myPos + new Vector3(0.5f, 0.5f, 0)));
-            
+
+            // If numFightersToSpawn is greater than 5, spawn the remaining fighters randomly around this object
+            for (int i = 5; i < numFightersToSpawn; i++) {
+                result.Add(myPos + new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(0.0f, 0.5f), 0));
+            }
+
             return result;
         }
-        
-        // Spawns a fighter as a child of shipsTransform. Similar to the SpawnShip method in Spawning.cs .
+
+        // Spawns a fighter as a child of shipsTransform. Similar to the SpawnShip() method in Spawning.cs .
         private void SpawnFighter(Vector3 position, Transform shipsTransform) {
             int fighterShipID = 0;
             GameObject newFighter = Instantiate(GameManager.Instance.alienShipPrefabs[fighterShipID], shipsTransform);
@@ -85,17 +89,19 @@ namespace AsterionArcade
         {
             // Get the health of this object through the fighter_enemy_health script attached to it
             int myHealth = this.GetComponent<fighter_enemy_health>().health;
+
             // Need to check cause of destruction
-            if ((myHealth <= 0) || inRangeOfPlayer)
-                {
-                List<Vector3> listOfSpawnPositions = generateListOfSpawnPositions();
-                // Set the transform component that fighters will spawn as a child of to be the parent of this object
-                Transform shipsTransform = transform.parent;
-                
-                for (int i = 0; i < numFightersToSpawn; i++)
-                {
-                    this.SpawnFighter(listOfSpawnPositions[i], shipsTransform);
-                }
+            if (myHealth > 0 && !inRangeOfPlayer) {
+                return;
+            }
+
+            List<Vector3> listOfSpawnPositions = generateListOfSpawnPositions();
+            // Set the transform component that fighters will spawn as a child of to be the parent of this object
+            Transform shipsTransform = transform.parent;
+
+            for (int i = 0; i < numFightersToSpawn; i++)
+            {
+                this.SpawnFighter(listOfSpawnPositions[i], shipsTransform);
             }
         }
     }
