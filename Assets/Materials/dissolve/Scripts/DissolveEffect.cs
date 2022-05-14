b02afb4623778caf735fe8e2679221be27087229
Calex Raffield <clxrffdman@gmail.com>
2022-05-14 13:51:58 -0700

    -added dissolve shader for enemy ships

---
+++ Assets/Materials/dissolve/Scripts/DissolveEffect.cs
@@ -0,0 +1,60 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveEffect : MonoBehaviour {
+
+    [SerializeField] private Material material;
+
+    private float dissolveAmount;
+    private float dissolveSpeed;
+    private bool isDissolving;
+
+    private void Start() {
+        if (material == null) {
+            material = transform.GetChild(0).GetComponent<SpriteRenderer>().material;
+        }
+    }
+
+    private void Update() {
+        if (isDissolving) {
+            dissolveAmount = Mathf.Clamp01(dissolveAmount + dissolveSpeed * Time.deltaTime);
+            material.SetFloat("_DissolveAmount", dissolveAmount);
+        } else {
+            dissolveAmount = Mathf.Clamp01(dissolveAmount - dissolveSpeed * Time.deltaTime);
+            material.SetFloat("_DissolveAmount", dissolveAmount);
+        }
+    }
+
+    public void StartDissolve(float dissolveSpeed, Color dissolveColor) {
+        material = transform.GetChild(0).GetComponent<SpriteRenderer>().material;
+        isDissolving = true;
+        material.SetColor("_DissolveColor", dissolveColor);
+        this.dissolveSpeed = dissolveSpeed;
+    }
+
+    public void StopDissolve(float dissolveSpeed, Color dissolveColor) {
+        material = transform.GetChild(0).GetComponent<SpriteRenderer>().material;
+        isDissolving = false;
+        material.SetColor("_DissolveColor", dissolveColor);
+        this.dissolveSpeed = dissolveSpeed;
+    }
+
+    public void SetEmpty()
+    {
+        material = transform.GetChild(0).GetComponent<SpriteRenderer>().material;
+        dissolveAmount = 1;
+        material.SetFloat("_DissolveAmount", dissolveAmount);
+
+    }
+
+    public void SetFull()
+    {
+        material = transform.GetChild(0).GetComponent<SpriteRenderer>().material;
+        dissolveAmount = 0;
+        material.SetFloat("_DissolveAmount", dissolveAmount);
+    }
+
+}
--- Assets/Scripts/AI_Scripts/fighter_enemy_health.cs
+++ Assets/Scripts/AI_Scripts/fighter_enemy_health.cs
@@ -6,22 +6,51 @@ namespace AsterionArcade
     public class fighter_enemy_health : BasicDamageable
     {
 
+        [SerializeField] DissolveEffect dissolveFX;
+        [SerializeField] Color dissolveColor;
+        Collider2D bodyCollider;
+
         [Header("SFX References")]
         [SerializeField] FMODUnity.EventReference alienDeathSFX;
 
+        public void Awake()
+        {
+            if (GetComponent<DissolveEffect>() != null)
+            {
+
+                dissolveFX = GetComponent<DissolveEffect>();
+            }
+            
+        }
         public override void Start()
         {
             base.Start();
             isAlien = true;
+            bodyCollider = GetComponent<Collider2D>();
+            if(dissolveFX != null)
+            {
+                
+                dissolveFX.SetEmpty();
+                dissolveFX.StopDissolve(2f, dissolveColor);
+            }
+            
         }
 
         public override void Death()
         {
             // Play the SFX that plays when the alien ship fucking explodes
             FMODUnity.RuntimeManager.PlayOneShot(alienDeathSFX.Guid);
+
             
+
+            if (dissolveFX != null)
+            {
+                dissolveFX.StartDissolve(2f, dissolveColor);
+            }
+
+            bodyCollider.enabled = false;
             base.Death();
-            Destroy(this.gameObject);
+            Destroy(this.gameObject,1);
         }
 
         public override void Disable()
